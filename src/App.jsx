418728e35346825f41ccf99c7585a3cf783b25ae
Natalia <kohen.natalia@gmail.com>
2024-05-06 22:04:21 -0400

    agregué archivos y rutas para encabezado

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-
+import Layout from './components/layout/Layout';
+import Home from './routes/home/Home';
 function App() {
-
-
   return (
     <>
-    
-        
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
