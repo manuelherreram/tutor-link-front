1c4080ea9f8577e12f385f8ab174140ae98f30e5
Natalia <kohen.natalia@gmail.com>
2024-05-07 18:46:15 -0400

    completando el header

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,11 +1,24 @@
-
+import './Header.css';
+import { Link } from 'react-router-dom';
+import { Button } from '@mui/material';
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
-  )
-}
+    <header className="container-header">
+      <div className="container-logo">
+        <Link to="/" className="element-header">
+          <img src="" alt="logotipo" />
+        </Link>
+        <Link to="/" className="element-header">
+          <h3>Tu camino hacia el éxito</h3>
+        </Link>
+      </div>
+      <div>
+        <Button className="element-header" variant="outlined">Crear cuenta</Button>
+        <Button className="element-header" variant="outlined">Iniciar sesión</Button>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
