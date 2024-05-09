e1cc16427c67ff392a676064aea64e701336d3e1
Natalia <kohen.natalia@gmail.com>
2024-05-09 08:04:17 -0400

    agregué global context y title al index

---
+++ src/components/globalContext.js
@@ -0,0 +1,18 @@
+import { createContext, useContext, useState } from 'react';
+
+export const ContextGlobal = createContext();
+
+
+
+export const ContextProvider = ({ children }) => {
+  
+    const[teacher,setTeacher]=useState()
+
+  return (
+    <ContextGlobal.Provider value={{ teacher,setTeacher }}>
+      {children}
+    </ContextGlobal.Provider>
+  );
+};
+
+export const useTutorState = () => useContext(ContextGlobal);
