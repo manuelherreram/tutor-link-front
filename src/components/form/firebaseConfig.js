5d29c757e20febe127622aaafd55dbf396d4f562
Natalia <kohen.natalia@gmail.com>
2024-05-10 20:19:20 -0400

    configuracion firebase

---
+++ src/components/form/firebaseConfig.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket:import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
+};
+const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+// Obtiene una referencia al almacenamiento de Firebase
+const storage = getStorage(app);
+
+// Exporta la referencia al almacenamiento de Firebase para que otros archivos puedan usarlo
+export { storage };
+
