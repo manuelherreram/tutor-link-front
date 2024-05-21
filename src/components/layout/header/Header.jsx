0db848650decefe43dda324b70e07cd49d92be3e
Natalia <kohen.natalia@gmail.com>
2024-05-20 21:20:24 -0400

    correción botones según actualización del figma

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,24 +1,34 @@
 import './Header.css';
 import { Link } from 'react-router-dom';
-import { Button } from '@mui/material';
 
 const Header = () => {
   return (
     <header className="container-header">
       <div className="container-logo">
         <Link to="/" className="element-header">
-          <img src="/tutor-link-logo.png" alt="logotipo" className='img-header' />
+          <img
+            src="/tutor-link-logo.png"
+            alt="logotipo"
+            className="img-header"
+          />
         </Link>
+      </div>
+
+      <nav>
         <Link to="/" className="element-header">
-          <h3>Tu camino hacia el éxito</h3>
+          <h4>Tu camino al éxito comienza aquí...</h4>
         </Link>
-      </div>
-      <div>
-        <Link to='/register'><Button id="btn" variant="contained">
-          Crear cuenta
-        </Button></Link>
-        <Link to='/login'><Button id="btn" variant="contained">Iniciar sesión</Button></Link>
-      </div>
+        <Link to="/register">
+          <button className="btn btn-sign" >
+           Registrar
+          </button>
+        </Link>
+        <Link to="/login">
+          <button className="btn btn-login" >
+           Conectar
+          </button>
+        </Link>
+      </nav>
     </header>
   );
 };
