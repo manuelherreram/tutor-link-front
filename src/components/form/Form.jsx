3b389917605d09ef29c78a126e46904db65706b9
Natalia <kohen.natalia@gmail.com>
2024-05-25 21:55:44 -0400

    correciones para caracteristicas

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { useFormik } from 'formik';
-// import { verificarDNIServidor } from '../../api/api';
 import * as Yup from 'yup';
 import ImageUploader from './ImageUploader';
 import { storage } from '../../firebase/firebaseConfig';
@@ -10,7 +9,7 @@ import Swal from 'sweetalert2';
 import { register } from '../../api/api';
 import { useNavigate } from 'react-router-dom';
 import './Form.css';
-import { useAuth } from '../../contexts/AuthContext'
+import { useAuth } from '../../contexts/AuthContext';
 
 const Form = () => {
   const [mostrar, setMostrar] = useState(true);
@@ -18,19 +17,19 @@ const Form = () => {
   const { idToken } = useAuth();
 
   const subjectOptions = [
-    { value: 1, label: 'Historia' },
-    { value: 2, label: 'Matemáticas' },
-    { value: 3, label: 'Inglés' },
-    { value: 4, label: 'Lenguaje' },
+    { value: 'Historia', label: 'Historia' },
+    { value: 'Matemáticas', label: 'Matemáticas' },
+    { value: 'Inglés', label: 'Inglés' },
+    { value: 'Lenguaje', label: 'Lenguaje' },
   ];
 
   const characteristicsOptions = [
     { id: 1, name: 'Licenciado en Educacion' },
-    { id: 2, name: 'Ingles' },
+    { id:2, name: 'Ingles' },
     { id: 3, name: 'Clase de Prueba' },
     { id: 4, name: 'Clases Presenciales' },
     { id: 5, name: 'Clases Grupales' },
-    { id: 6, name: 'SuperProfe' },
+    { id: 6, name: 'SuperProfe',url:"" },
   ];
 
   const {
@@ -57,7 +56,7 @@ const Form = () => {
               const storageRef = storage.ref(`images/${image.name}`);
               await storageRef.put(image);
               const url = await storageRef.getDownloadURL();
-              return { id: index + 1, url, title: `Imagen ${index + 1} del Profesor` };
+              return { url, title: `Imagen ${index + 1} del Profesor` };
             })
           );
 
@@ -66,21 +65,20 @@ const Form = () => {
 
         // Construcción del objeto final
         const finalData = {
-          id: 1, // Puedes generar un ID único aquí o manejarlo de otra forma
           name: data.name,
           dni: data.dni,
           description: data.description,
           images: data.images,
           subject: {
-            id: data.subject,
-            title: subjectOptions.find(option => option.value === data.subject)?.label,
+            title: data.subject,
           },
           characteristics: characteristicsOptions.filter(option =>
             data.characteristics.includes(option.id)
           ),
+        
         };
 
-        const response = await register(finalData,idToken);
+        const response = await register(finalData, idToken);
         setMostrar(false);
         console.log('Respuesta del servidor:', response);
         Swal.fire({
@@ -104,10 +102,6 @@ const Form = () => {
         .min(8, 'El DNI debe tener por lo menos 8 caracteres')
         .max(11, 'El DNI debe tener máximo 11 caracteres')
         .required('El campo es requerido'),
-        // .test('dni-unico', '¡El DNI ya está en uso!', async function (value) {
-        //   const dniNoExistente = await verificarDNIServidor(value);
-        //   return dniNoExistente;
-        // }),
       description: Yup.string(),
       subject: Yup.string().required('Seleccione un tema'),
     }),
@@ -117,86 +111,85 @@ const Form = () => {
   return (
     <main>
       {mostrar && (
-        <>
-          <form className="container-form" onSubmit={handleSubmitProfessor}>
-            <h4>Registro del Tutor</h4>
-            <TextField
-              type="text"
-              onChange={handleChangeProfessor}
-              name="name"
-              label="Ingrese su nombre"
-              variant="outlined"
-              error={errorsProfessor.name ? true : false}
-              helperText={errorsProfessor.name}
-            />
-            <TextField
-              type="text"
-              onChange={handleChangeProfessor}
-              name="dni"
-              label="Ingrese el DNI del profesor"
-              variant="outlined"
-              autoComplete="off"
-              error={errorsProfessor.dni ? true : false}
-              helperText={errorsProfessor.dni}
-            />
-            <TextField
-              type="text"
-              onChange={handleChangeProfessor}
-              name="description"
-              label="Ingrese una descripción"
-              multiline
-              rows={4}
-              variant="outlined"
-              autoComplete="off"
-              error={errorsProfessor.description ? true : false}
-              helperText={errorsProfessor.description}
-            />
-            <InputLabel id="subject-select-label">Selecciona la asignatura</InputLabel>
-            <Select
-              labelId="subject-select-label"
-              id="subject-select"
-              value={valuesProfessor.subject}
-              name="subject"
-              label="Subject"
-              onChange={handleChangeProfessor}
-              displayEmpty
-              inputProps={{ 'aria-label': 'Subject' }}
-              fullWidth
-            >
-              {subjectOptions.map((option) => (
-                <MenuItem key={option.value} value={option.value}>
-                  {option.label}
-                </MenuItem>
-              ))}
-            </Select>
-
-            <ImageUploader folderName={valuesProfessor.name} setFieldValue={setFieldValue} />
-
-            <h4>Características del Tutor</h4>
-            {characteristicsOptions.map((option) => (
-              <label key={option.id}>
-                <input
-                  type="checkbox"
-                  onChange={() => {
-                    const updatedCharacteristics = valuesProfessor.characteristics.includes(option.id)
-                      ? valuesProfessor.characteristics.filter(id => id !== option.id)
-                      : [...valuesProfessor.characteristics, option.id];
-                    setFieldValue('characteristics', updatedCharacteristics);
-                  }}
-                  checked={valuesProfessor.characteristics.includes(option.id)}
-                />{' '}
-                {option.name}
-              </label>
+        <form className="container-form" onSubmit={handleSubmitProfessor}>
+          <h4>Registro del Tutor</h4>
+          <TextField
+            type="text"
+            onChange={handleChangeProfessor}
+            name="name"
+            label="Ingrese su nombre"
+            variant="outlined"
+            error={errorsProfessor.name ? true : false}
+            helperText={errorsProfessor.name}
+          />
+          <TextField
+            type="text"
+            onChange={handleChangeProfessor}
+            name="dni"
+            label="Ingrese el DNI del profesor"
+            variant="outlined"
+            autoComplete="off"
+            error={errorsProfessor.dni ? true : false}
+            helperText={errorsProfessor.dni}
+          />
+          <TextField
+            type="text"
+            onChange={handleChangeProfessor}
+            name="description"
+            label="Ingrese una descripción"
+            multiline
+            rows={4}
+            variant="outlined"
+            autoComplete="off"
+            error={errorsProfessor.description ? true : false}
+            helperText={errorsProfessor.description}
+          />
+          <InputLabel id="subject-select-label">Selecciona la asignatura</InputLabel>
+          <Select
+            labelId="subject-select-label"
+            id="subject-select"
+            value={valuesProfessor.subject}
+            name="subject"
+            label="Subject"
+            onChange={handleChangeProfessor}
+            displayEmpty
+            inputProps={{ 'aria-label': 'Subject' }}
+            fullWidth
+          >
+            {subjectOptions.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
             ))}
+          </Select>
 
-            <Button type="submit" variant="contained" color="primary">
-              Enviar
-            </Button>
-          </form>
-        </>
+          <ImageUploader folderName={valuesProfessor.name} setFieldValue={setFieldValue} />
+
+          <h4>Características del Tutor</h4>
+          {characteristicsOptions.map((option) => (
+            <label key={option.id}>
+              <input
+                type="checkbox"
+                onChange={() => {
+                  const updatedCharacteristics = valuesProfessor.characteristics.includes(option.id)
+                    ? valuesProfessor.characteristics.filter(id => id !== option.id)
+                    : [...valuesProfessor.characteristics, option.id];
+                  setFieldValue('characteristics', updatedCharacteristics);
+                }}
+                checked={valuesProfessor.characteristics.includes(option.id)}
+              />{' '}
+              {option.name}
+            </label>
+          ))}
+
+          <Button type="submit" variant="contained" color="primary">
+            Enviar
+          </Button>
+        </form>
       )}
     </main>
   );
 };
 
 export default Form;
+
