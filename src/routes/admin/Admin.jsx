390bb9ea263cd3d0ff467dbbaaeda137119f7062
paulasofiagc <paulasofiagc@gmail.com>
2024-05-29 18:17:06 -0400

    Se vuelve agregar al panel users

--- src/routes/admin/Admin.jsx
+++ src/routes/admin/Admin.jsx
@@ -6,103 +6,105 @@ import { useNavigate } from 'react-router-dom'
 import './Admin.css'
 
 const Admin = () => {
-  const items = [
-    {
-      label: 'Todos los Profesores',
-      key: 'profesores',
-      icon: <WalletOutlined />,
-    },
-    {
-      label: 'Agregar un Profesor',
-      key: 'agregar',
-      icon: <UserAddOutlined />,
-    },
-    {
-      label: 'Administrar características',
-      key: 'caracteristicas',
-      icon: <UserAddOutlined />, 
-    },
-    {
-      label: 'Administrar categorias',
-      key: 'categorias',
-      icon: <UserAddOutlined />,
-    },
-    {
-      label: 'Todos los usuarios ',
-      key: 'usuarios',
-      icon:  <WalletOutlined />,
-      
-    },
-  ]
+    const items = [
+        {
+            label: 'Todos los Profesores',
+            key: 'profesores',
+            icon: <WalletOutlined />,
+        },
+        {
+            label: 'Agregar un Profesor',
+            key: 'agregar',
+            icon: <UserAddOutlined />,
+        },
+        {
+            label: 'Administrar características',
+            key: 'caracteristicas',
+            icon: <UserAddOutlined />,
+        },
+        {
+            label: 'Administrar categorias',
+            key: 'categorias',
+            icon: <UserAddOutlined />,
+        },
+        {
+            label: 'Todos los usuarios ',
+            key: 'usuarios',
+            icon: <WalletOutlined />,
+        },
+    ]
 
-  const [current, setCurrent] = useState('')
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768)
-  const [showPanel, setShowPanel] = useState(false) // State to control the panel visibility
-  const navigate = useNavigate()
+    const [current, setCurrent] = useState('')
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 768)
+    const [showPanel, setShowPanel] = useState(false) // State to control the panel visibility
+    const navigate = useNavigate()
 
-  useEffect(() => {
-    const handleResize = () => setIsMobile(window.innerWidth <= 768)
-    window.addEventListener('resize', handleResize)
-    return () => window.removeEventListener('resize', handleResize)
-  }, [])
+    useEffect(() => {
+        const handleResize = () => setIsMobile(window.innerWidth <= 768)
+        window.addEventListener('resize', handleResize)
+        return () => window.removeEventListener('resize', handleResize)
+    }, [])
 
-  const handleMenuClick = (e) => {
-    setCurrent(e.key)
+    const handleMenuClick = e => {
+        setCurrent(e.key)
 
-    if (e.key === 'profesores') {
-      setShowPanel(true)
-    } else if (e.key === 'agregar') {
-      navigate('/admin/new')
-      setShowPanel(false)
-    }else if(e.key === 'caracteristicas'){
-      navigate('/admin/characteristics')
-      setShowPanel(false)
-
-
-    }else if(e.key === 'categorias'){
-      navigate('/admin/categories')
-      setShowPanel(false)
-
-
+        if (e.key === 'profesores') {
+            setShowPanel(true)
+        } else if (e.key === 'agregar') {
+            navigate('/admin/new')
+            setShowPanel(false)
+        } else if (e.key === 'caracteristicas') {
+            navigate('/admin/characteristics')
+            setShowPanel(false)
+        }else if (e.key === 'usuarios'){
+          navigate('/admin/users')
+          setShowPanel(false)
+        }
+        else if (e.key === 'categorias') {
+            navigate('/admin/categories')
+            setShowPanel(false)
+        }
     }
 
-  }
-
-  const handleGoHome = () => {
-    navigate('/') // Navigate to the root path
-  }
-
-  const renderContent = () => {
-    if (isMobile) {
-      return (
-        <div className="container-not-available">
-          <Result
-            status="warning"
-            title="Panel no disponible en dispositivos móviles."
-            extra={
-              <Button type="primary" key="console" onClick={handleGoHome}>
-                Ir al Inicio
-              </Button>
-            }
-          />
-        </div>
-      )
-    } else {
-      return (
-        <>
-          <Menu
-            onClick={handleMenuClick}
-            selectedKeys={[current]}
-            mode="horizontal"
-            items={items}
-          />
-          {showPanel && <Panel showPanel={showPanel} />}
-        </>
-      )
+    const handleGoHome = () => {
+        navigate('/') // Navigate to the root path
     }
-  }
 
-  return <div className="admin-container">{renderContent()}</div>
+    const renderContent = () => {
+        if (isMobile) {
+            return (
+                <div className="container-not-available">
+                    <Result
+                        status="warning"
+                        title="Panel no disponible en dispositivos móviles."
+                        extra={
+                            <Button
+                                type="primary"
+                                key="console"
+                                onClick={handleGoHome}
+                            >
+                                Ir al Inicio
+                            </Button>
+                        }
+                    />
+                </div>
+            )
+        } else {
+            return (
+                <>
+                    <Menu
+                        onClick={handleMenuClick}
+                        selectedKeys={[current]}
+                        mode="horizontal"
+                        items={items}
+                    />
+                    {showPanel && <Panel showPanel={showPanel} />}
+                </>
+            )
+        }
+    }
+
+    return <div className="admin-container">{renderContent()}</div>
 }
 
 export default Admin
