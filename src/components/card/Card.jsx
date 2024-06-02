971a0fa3efdb676b4ae0f3f29c9226c84f8df98c
Natalia <kohen.natalia@gmail.com>
2024-06-01 22:03:49 -0400

    agregué icono favorito

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,12 +1,23 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import './Card.css'
+import { HeartOutlined,  HeartFilled} from '@ant-design/icons';
 
 const Card = ({name, category, image, description, id}) => {
+    const [isFavorited, setIsFavorited] = useState(false);
+    
+    const toggleFavorite = () => {
+        setIsFavorited(!isFavorited);
+    };
 
-    return (
-        <div className="card">
+   
+return(
+    <div className="card">
             <div className="card-image">
                 <img src={image} alt="" />
+                <div className="favorite-icon" onClick={toggleFavorite}>
+                    {isFavorited ? <HeartFilled style={{ color: 'red' }} /> : <HeartOutlined style={{ color: 'white' }} />}
+                </div>
             </div>
             <div className='card-body'>
                 <div className='card-name'>
@@ -18,12 +29,12 @@ const Card = ({name, category, image, description, id}) => {
                 <div className='card-description'>
                     <p>{ description }</p>
                 </div>
-                
-                
             </div>
+            <div className='card-footer'>
                 <Link to={`detalle/${id}`}><button className='card-button'>Conectar</button></Link>
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default Card
+export default Card;
