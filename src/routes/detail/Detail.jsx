95a37e1c8b169a108f8738aa145540325882fb58
Natalia <kohen.natalia@gmail.com>
2024-06-05 15:44:04 -0400

    Agregué calendario a detail

--- src/routes/detail/Detail.jsx
+++ src/routes/detail/Detail.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { getDataById } from '../../api/api';
 import { useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
-import { Button } from 'antd';
+import { Button, DatePicker, Tooltip } from 'antd';
 import { ArrowLeftOutlined } from '@ant-design/icons';
 import './Detail.css';
 import Modal from 'react-modal';
@@ -15,6 +15,7 @@ const Detail = () => {
   const [teacherSelected, setTeacherSelected] = useState();
   const [galleryImages, setGalleryImages] = useState([]);
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const { RangePicker } = DatePicker;
 
   const openModal = () => {
     setModalIsOpen(true);
@@ -45,6 +46,26 @@ const Detail = () => {
       setGalleryImages(formattedImages);
     }
   }, [teacherSelected]);
+
+//Función para calendario
+  const disabledDate = (current) => {
+    // Suponiendo que las fechas disponibles son del 10 al 20 de cada mes
+    return current && (current.date() < 10 || current.date() > 20);
+  };
+  const renderExtraFooter = () => (
+    <div>
+      <Tooltip title="Fechas no disponibles" placement="bottom">
+        <div
+          style={{ background: '#ff4d4f', width: '100%', height: '10px' }}
+        ></div>
+      </Tooltip>
+      <Tooltip title="Fechas disponibles" placement="bottom">
+        <div
+          style={{ background: '#52c41a', width: '100%', height: '10px' }}
+        ></div>
+      </Tooltip>
+    </div>
+  );
 
   return (
     <div>
@@ -90,8 +111,19 @@ const Detail = () => {
                 <Button type="primary" className="more" onClick={openModal}>
                   ver más
                 </Button>
+                <div style={{ padding: 24 }}>
+                  <h3>Fechas disponibles Tutor</h3>
+              <RangePicker
+                disabledDate={disabledDate}
+                renderExtraFooter={renderExtraFooter}
+                onChange={(dates, dateStrings) =>
+                  console.log(dates, dateStrings)
+                }
+              />
+            </div>
               </div>
             </section>
+           
             <div>
               <h3> Características: </h3>
               <div className="cont-other-img">
@@ -111,7 +143,9 @@ const Detail = () => {
           {/* Contenido de la modal */}
           <h2>Galería de Imágenes</h2>
           {galleryImages.length > 0 && <ImageGallery items={galleryImages} />}
-          <Button type="primary" onClick={closeModal}>Cerrar</Button>
+          <Button type="primary" onClick={closeModal}>
+            Cerrar
+          </Button>
         </Modal>
       </div>
     </div>
