980ae3da83e53414990d57a6e4ef6760f5187cd6
Natalia <kohen.natalia@gmail.com>
2024-06-11 11:25:38 -0400

    creacuón archivos para reservas

--- src/api/apiReservations.js
+++ src/api/apiReservations.js
@@ -1,18 +1,22 @@
-import axios from "axios"
+import axios from 'axios';
 import dayjs from 'dayjs';
-const BASE_URL='http://localhost:8080/api'
 
-// Disponibilidad de un profesor
+const BASE_URL = 'http://localhost:8080/api';
+// const BASE_URL = "http://44.193.72.252:8080/api";
+
+// Obtener disponibilidad de un profesor
 export const getAvailabilitiesById = async (id) => {
-    try {
-      const response = await axios.get(`${BASE_URL}/availabilities/teacher/${id}`);
-      return response.data;
-    } catch (error) {
-      console.error('Error fetching availabilities:', error);
-      throw error;
-    }
-  };
-  
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/availabilities/teacher/${id}`
+    );
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching availabilities:', error);
+    throw error;
+  }
+};
+//Reservar hora con un profesor
 export const addReservation = async (userId, teacherId, start, end) => {
   let res = await axios.post(`${BASE_URL}/reservations`, {
     userId,
@@ -20,6 +24,15 @@ export const addReservation = async (userId, teacherId, start, end) => {
     startTime: start.format('YYYY-MM-DDTHH:mm:ss'),
     endTime: end.format('YYYY-MM-DDTHH:mm:ss'),
     status: 'PENDING',
+  });
+  return res.data;
+};
+export const addSchedule = async (teacherId, date, startTime, endTime) => {
+  let res = await axios.post(`${BASE_URL}/availabilities`, {
+    teacherId,
+    date,
+    startTime,
+    endTime
   });
   return res.data;
 };
