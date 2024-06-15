fb916918cfb1a05394abce32c3667da274cdb845
Manuel Herrera <manuel.herrera.m@gmail.com>
2024-06-15 13:10:05 -0400

    adds WhatsApp btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,108 +1,117 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Detail from './routes/detail/Detail';
-import Layout from './components/layout/Layout';
-import Home from './routes/home/Home';
-import Admin from './routes/admin/Admin.jsx';
-import { TeacherContextProvider } from './contexts/TeacherContext.jsx';
-import NewTeacher from './routes/newTeacher/NewTeacher';
-import Login from './routes/login/Login.jsx';
-import Characteristics from './routes/characteristics/Characteristics.jsx';
-import Register from './routes/register/Register.jsx';
-import { AuthProvider } from './contexts/AuthContext.jsx';
-import AdminRoute from './routes/AdminRoute.jsx';
-import PrivateRoute from './routes/PrivateRoutes.jsx';
-import CategoryFilters from './routes/catFilter/CategoryFilters.jsx';
-import Categories from './routes/categories/Categories.jsx';
-import Users from './components/admin/users/Users.jsx';
-import Profile from './routes/Profile/Profile.jsx';
-import { FavoriteProvider } from './contexts/FavoriteContexts.jsx';
-import Favorites from './components/favorites/Favorites.jsx';
-import Reservations from './routes/reservations/Reservations.jsx';
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Detail from "./routes/detail/Detail";
+import Layout from "./components/layout/Layout";
+import Home from "./routes/home/Home";
+import Admin from "./routes/admin/Admin.jsx";
+import { TeacherContextProvider } from "./contexts/TeacherContext.jsx";
+import NewTeacher from "./routes/newTeacher/NewTeacher";
+import Login from "./routes/login/Login.jsx";
+import Characteristics from "./routes/characteristics/Characteristics.jsx";
+import Register from "./routes/register/Register.jsx";
+import { AuthProvider } from "./contexts/AuthContext.jsx";
+import AdminRoute from "./routes/AdminRoute.jsx";
+import PrivateRoute from "./routes/PrivateRoutes.jsx";
+import CategoryFilters from "./routes/catFilter/CategoryFilters.jsx";
+import Categories from "./routes/categories/Categories.jsx";
+import Users from "./components/admin/users/Users.jsx";
+import Profile from "./routes/Profile/Profile.jsx";
+import { FavoriteProvider } from "./contexts/FavoriteContexts.jsx";
+import Favorites from "./components/favorites/Favorites.jsx";
+import Reservations from "./routes/reservations/Reservations.jsx";
 function App() {
+  useEffect(() => {
+    if (Notification.permission !== "granted") {
+      Notification.requestPermission();
+    }
+  }, []);
   return (
     <>
       <BrowserRouter>
         <TeacherContextProvider>
           <AuthProvider>
             <FavoriteProvider>
-            <Routes>
-              {/* Rutas no protegidas */}
-              <Route element={<Layout />}>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/filter/:category" element={<CategoryFilters />} />
-                <Route path="/detalle/:id" element={<Detail />} />
-            
-                {/* Rutas protegidas sólo para logueados */}
-                <Route
-                  path="/Profile"
-                  element={
-                    <PrivateRoute>
-                      <Profile />
-                    </PrivateRoute>
-                  }
-                />
-                <Route
-                  path="/favorites"
-                  element={
-                    <PrivateRoute>
-                      <Favorites/>
-                    </PrivateRoute>
-                  }
-                />
+              <Routes>
+                {/* Rutas no protegidas */}
+                <Route element={<Layout />}>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/register" element={<Register />} />
                   <Route
-                  path="/reservations"
-                  element={
-                    <PrivateRoute>
-                      <Reservations/>
-                    </PrivateRoute>
-                  }
-                />
-                {/* Rutas protegidas para administradores */}
-                <Route
-                  path="/admin"
-                  element={
-                    <AdminRoute>
-                      <Admin />
-                    </AdminRoute>
-                  }
-                />
+                    path="/filter/:category"
+                    element={<CategoryFilters />}
+                  />
+                  <Route path="/detalle/:id" element={<Detail />} />
 
-                <Route
-                  path="/admin/new"
-                  element={
-                    <AdminRoute>
-                      <NewTeacher />
-                    </AdminRoute>
-                  }
-                />
-                <Route
-                  path="/admin/characteristics"
-                  element={
-                    <AdminRoute>
-                      <Characteristics />
-                    </AdminRoute>
-                  }
-                />
-                <Route
-                  path="/admin/users"
-                  element={
-                    <AdminRoute>
-                      <Users />
-                    </AdminRoute>
-                  }
-                />
-                <Route
-                  path="/admin/categories"
-                  element={
-                    <AdminRoute>
-                      <Categories />
-                    </AdminRoute>
-                  }
-                />
-              </Route>
-            </Routes>
+                  {/* Rutas protegidas sólo para logueados */}
+                  <Route
+                    path="/Profile"
+                    element={
+                      <PrivateRoute>
+                        <Profile />
+                      </PrivateRoute>
+                    }
+                  />
+                  <Route
+                    path="/favorites"
+                    element={
+                      <PrivateRoute>
+                        <Favorites />
+                      </PrivateRoute>
+                    }
+                  />
+                  <Route
+                    path="/reservations"
+                    element={
+                      <PrivateRoute>
+                        <Reservations />
+                      </PrivateRoute>
+                    }
+                  />
+                  {/* Rutas protegidas para administradores */}
+                  <Route
+                    path="/admin"
+                    element={
+                      <AdminRoute>
+                        <Admin />
+                      </AdminRoute>
+                    }
+                  />
+
+                  <Route
+                    path="/admin/new"
+                    element={
+                      <AdminRoute>
+                        <NewTeacher />
+                      </AdminRoute>
+                    }
+                  />
+                  <Route
+                    path="/admin/characteristics"
+                    element={
+                      <AdminRoute>
+                        <Characteristics />
+                      </AdminRoute>
+                    }
+                  />
+                  <Route
+                    path="/admin/users"
+                    element={
+                      <AdminRoute>
+                        <Users />
+                      </AdminRoute>
+                    }
+                  />
+                  <Route
+                    path="/admin/categories"
+                    element={
+                      <AdminRoute>
+                        <Categories />
+                      </AdminRoute>
+                    }
+                  />
+                </Route>
+              </Routes>
             </FavoriteProvider>
           </AuthProvider>
         </TeacherContextProvider>
--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -1,48 +1,51 @@
-import { useEffect, useState } from 'react';
-import Categories from '../../components/categoriesSection/CategoriesSection';
-import Search from '../../components/search/Search';
-import Card from '../../components/card/Card';
-import './Home.css';
+import { useEffect, useState } from "react";
+import Categories from "../../components/categoriesSection/CategoriesSection";
+import Search from "../../components/search/Search";
+import Card from "../../components/card/Card";
+import WhatsAppButton from "../../components/whatsappFloatingBtn/WhatsAppButton";
+import "./Home.css";
 
 const Home = () => {
-    const [randomTeachers, setRandomTeachers] = useState([]);
-    const [searchResults, setSearchResults] = useState([]);
+  const [randomTeachers, setRandomTeachers] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
 
-    useEffect(() => {
-        fetch('http://localhost:8080/api/public/index')
-            .then(res => res.json())
-            .then(data => setRandomTeachers(data));
-    }, []);
+  useEffect(() => {
+    fetch("http://localhost:8080/api/public/index")
+      .then((res) => res.json())
+      .then((data) => setRandomTeachers(data));
+  }, []);
 
-    const handleSearchResults = (results) => {
-        setSearchResults(results);
-    };
+  const handleSearchResults = (results) => {
+    setSearchResults(results);
+  };
 
-    const teachersToDisplay = searchResults.length > 0 ? searchResults : randomTeachers;
+  const teachersToDisplay =
+    searchResults.length > 0 ? searchResults : randomTeachers;
 
-    return (
-        <div className="container-home">
-            <div className="container-utilities">
-                <div className="container-title">
-                    <h2>Tu camino al éxito</h2>
-                </div>
-                <Search onSearchResults={handleSearchResults} />
-                <Categories />
-            </div>
-            <div className="container-cards">
-                {teachersToDisplay.map(teacher => (
-                    <Card 
-                        key={teacher.id} 
-                        name={teacher.name} 
-                        category={teacher.subject.title} 
-                        image={teacher.images[0]?.url} 
-                        description={teacher.description} 
-                        id={teacher.id} 
-                    />
-                ))}
-            </div>
+  return (
+    <div className="container-home">
+      <div className="container-utilities">
+        <div className="container-title">
+          <h2>Tu camino al éxito</h2>
         </div>
-    );
+        <Search onSearchResults={handleSearchResults} />
+        <Categories />
+      </div>
+      <div className="container-cards">
+        {teachersToDisplay.map((teacher) => (
+          <Card
+            key={teacher.id}
+            name={teacher.name}
+            category={teacher.subject.title}
+            image={teacher.images[0]?.url}
+            description={teacher.description}
+            id={teacher.id}
+          />
+        ))}
+      </div>
+      <WhatsAppButton />
+    </div>
+  );
 };
 
 export default Home;
